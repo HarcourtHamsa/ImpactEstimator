4c2d4ee052df358aa7fe706725b5c7d743f45ef7
Harcourt Hamsa <hamsaharcourt@gmail.com>
2020-04-11 16:57:24 +0100

    Updates

---
+++ precache-manifest.5fb879890507bf26ea7fb9d3bce09b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3627e87b638001ec772f6a83edc1aa2d",
+    "url": "/Covid19-Impact-Estimator-Web-App/index.html"
+  },
+  {
+    "revision": "832fe783c47580b79428",
+    "url": "/Covid19-Impact-Estimator-Web-App/static/css/main.7bfd9ee4.chunk.css"
+  },
+  {
+    "revision": "20164bae076ca6285d72",
+    "url": "/Covid19-Impact-Estimator-Web-App/static/js/2.9cbb552f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid19-Impact-Estimator-Web-App/static/js/2.9cbb552f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "832fe783c47580b79428",
+    "url": "/Covid19-Impact-Estimator-Web-App/static/js/main.f053d663.chunk.js"
+  },
+  {
+    "revision": "2eedb3a1f1e4688c9e54",
+    "url": "/Covid19-Impact-Estimator-Web-App/static/js/runtime-main.8603b5c4.js"
+  }
+]);
