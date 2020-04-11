5e23ede10a6ca3f178386dc0436e95858150814f
Harcourt Hamsa <hamsaharcourt@gmail.com>
2020-04-11 16:49:23 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "./components/Nav";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <div className="container mt-5">
+        <p className="mb-5 font-weight-bold">COVID19 IMPACT ESTIMATOR</p>
+
+        <div className="row">
+          <div className="col-xl-6">
+            <dorm>
+              <div className="form-control-group">
+                <div className="form-row">
+                  <div className="col-xl-6">
+                    <label htmlFor="#population" className="">
+                      Population
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control mb-3"
+                      id="population"
+                      data-population
+                    />
+                  </div>
+                  <div className="col-xl-6">
+                    <label htmlFor="#population" className="">
+                      Time to elapse
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control mb-3"
+                      data-time-to-elapse
+                    />
+                  </div>
+                </div>
+
+                <div className="form-row">
+                  <div className="col-xl-6">
+                    {" "}
+                    <label htmlFor="#population" className="">
+                      Reported cases
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control mb-3"
+                      data-reported-case
+                    />
+                  </div>
+                  <div className="col-xl-6">
+                    {" "}
+                    <label htmlFor="#population" className="">
+                      Hostpital beds
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control mb-3 "
+                      data-total-hospital-beds
+                    />
+                  </div>
+                </div>
+
+                <label htmlFor="#population" className="">
+                  Period type
+                </label>
+                <select
+                  className="browser-default custom-select mb-5"
+                  data-period-type
+                >
+                  <option value="2" selected>
+                    Days
+                  </option>
+                  <option value="3">Weeks</option>
+                  <option value="4">Months</option>
+                </select>
+
+                <button
+                  type="submit"
+                  className="btn p-3  pl-5 pr-5 rounded-0 btn-block"
+                  data-go-estimate
+                >
+                  Get Estimate
+                </button>
+              </div>
+            </dorm>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div>
+      <nav
+        className="navbar navbar-expand-md navbar-light p-3  mb-4 "
+        id="navbar"
+      >
+          <p className="h5 font-weight-bold text-warning">#BuildforSDG Challenge 2020</p>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
